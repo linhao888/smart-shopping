4005847a8248a164368c2c00785ffe55cc77264c
Dai <2035721725@qq.com>
2024-10-09 22:53:37 +0800

    更新的dist

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/smart-shopping/' // 替换为你的 GitHub 仓库名
-    : '/'
+  publicPath: './'
 })
