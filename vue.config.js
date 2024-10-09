c40beaffd9851e24dc3c5107c61e805ac2ac53b4
Dai <2035721725@qq.com>
2024-10-09 22:08:29 +0800

    1.1提交，修复了不能在github上运行的问题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/smart-shopping/' // 替换为你的 GitHub 仓库名
+    : '/'
 })
