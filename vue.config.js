8e213493e410b9588e1561cf37bea98fe3019ac1
Dai <2035721725@qq.com>
2024-10-11 11:35:31 +0800

    1.9

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: './'
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/smart-shopping/' // 替换为你的 GitHub 仓库名
-    : '/'
+  publicPath: './'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/smart-shopping/' // 替换为你的 GitHub 仓库名
+  //   : './'
 
   // css: {
   //   loaderOptions: {
